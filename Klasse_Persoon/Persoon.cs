1aa48c8ca9833ce7b0d6511eb0910f717ad412a9
Tom Adriaens <tomadriaens@hotmail.com>
2024-05-28 14:58:59 +0200

    Add project files.

---
+++ Klasse_Persoon/Persoon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasse_Persoon
+{
+    public class Persoon
+    {
+        // Velden 
+        String _voornaam = "John";
+        String _achternaam = "Doe";
+
+        // Properties (Eigenschappen)
+        public String PropVoornaam
+        {
+            get { return _voornaam; } 
+            set { _voornaam = value; }
+        }
+
+        public String PropAchternaam
+        {
+            get { return _achternaam; }
+            set { _achternaam = value; }
+        }
+
+        // Methoden 
+        /// <summary>
+        /// Deze functie stuurt de volledige naam (voornaam achternaam) terug
+        /// </summary>
+        /// <returns></returns>
+        public String VolledigeNaam ()
+        {
+            return $"{_voornaam} {_achternaam}"; 
+        }
+
+        /// <summary>
+        /// Deze functie stuurt het mailadres weer in de vorm voornaam.achternaam@romerocollege.be
+        /// </summary>
+        /// <returns></returns>
+        public String Mailadres()
+        {
+            return $"{_voornaam}.{_achternaam}@romerocollege.be".ToLower();
+        }
+
+        // Constructors
+        public Persoon() { }
+
+        public Persoon(string vn, string an) 
+        { 
+            _voornaam = vn;
+            _achternaam = an;
+        }
+    }
+}
